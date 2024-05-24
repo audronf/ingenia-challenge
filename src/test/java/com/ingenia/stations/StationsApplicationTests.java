479a2456804a9cab9c2aa653280ce66d9d75e2e0
Audronf <federicofasitella@gmail.com>
2024-05-24 15:57:34 -0300

    chore: project kickoff

---
+++ src/test/java/com/ingenia/stations/StationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingenia.stations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
